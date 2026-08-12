22c55eb96b65b89cb73e2da9b7e69d43026ed61b
Murilo Tamura <murilotamura@gmail.com>
2026-08-12 13:09:28 -0300

    initial commit

---
+++ src/main/java/com/br/benditopercal/bendito_percal/BenditoPercalApplication.java
@@ -0,0 +1,13 @@
+package com.br.benditopercal.bendito_percal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenditoPercalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenditoPercalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/benditopercal/bendito_percal/BenditoPercalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.benditopercal.bendito_percal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenditoPercalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
